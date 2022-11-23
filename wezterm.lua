56e3e8daed95fe06c240fd936b7de8553bf8bba6
Nahua Kang <kangnahua@gmail.com>
2022-11-23 17:45:27 +0100

    Add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,48 @@
+-- Run the following to set up on a new machine
+-- $ mkdir ~/.config/wezterm
+-- $ ln -s ~/dotfiles/wezterm.lua ~/.config/wezterm/wezterm.lua
+
+local wezterm = require 'wezterm'
+
+return {
+    color_scheme = "Gruvbox Dark",
+
+    font = wezterm.font_with_fallback{
+        'Comic Code Ligatures',
+        'FiraCode Nerd Font',
+        'JetBrains Mono',
+    },
+
+    initial_cols=160,
+    initial_rows=48,
+    
+    leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+    keys = {
+        { key = 'a', mods = 'LEADER|CTRL',  action = wezterm.action.SendString '\x01' },
+        { key = '|', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain'} },
+        { key = '-', mods = 'LEADER',       action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain'} },
+        { key = 'z', mods = 'LEADER',       action = "TogglePaneZoomState" },
+        { key = "t", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"} },
+        { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"} },
+        { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"} },
+        { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"} },
+        { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"} },
+        { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}} },
+        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}} },
+        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}} },
+        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}} },
+        { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0} },
+        { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1} },
+        { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2} },
+        { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3} },
+        { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4} },
+        { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5} },
+        { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6} },
+        { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7} },
+        { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8} },
+    },
+
+    scrollback_lines = 3500,
+
+    warn_about_missing_glyphs=false,
+}
