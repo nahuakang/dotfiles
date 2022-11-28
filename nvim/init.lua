06130138c1f14274c11f343341390036a4ffdcc2
Nahua Kang <kangnahua@gmail.com>
2022-11-28 11:23:08 +0100

    Update nvim tresitter configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,13 +45,13 @@ require('lualine').setup {
 ----------------
 
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "json", "lua", "rust", "toml" },
+    ensure_installed = { "json", "lua", "rust", "toml", "zig" },
     auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting=false,
     },
-    ident = { enable = true }, 
+    ident = { enable = true },
     rainbow = {
         enable = true,
         extended_mode = true,
